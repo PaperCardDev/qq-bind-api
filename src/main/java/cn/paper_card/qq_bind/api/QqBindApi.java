686d78979b4b3e93609bca69cb5e55f67a566856
Paper99 <1398872296@qq.com>
2024-06-28 18:40:18 +0800

    2.1 修正添加和删除的API

--- src/main/java/cn/paper_card/qq_bind/api/QqBindApi.java
+++ src/main/java/cn/paper_card/qq_bind/api/QqBindApi.java
@@ -8,9 +8,16 @@ import java.util.UUID;
 @SuppressWarnings("unused")
 public interface QqBindApi {
 
+    @Deprecated
     void addBind(@NotNull UUID uuid, long qq, @NotNull String remark) throws Exception;
 
+
+    @NotNull BindInfo addBind2(@NotNull UUID uuid, long qq, @NotNull String remark) throws Exception;
+
+    @Deprecated
     boolean removeBind(@NotNull UUID uuid, long qq) throws Exception;
+
+    @Nullable BindInfo removeBind(@NotNull UUID uuid) throws Exception;
 
     @Nullable BindInfo queryByUuid(@NotNull UUID uuid) throws Exception;
 
